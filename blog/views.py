e207e6d9c096e68200d6d2065f5cb2ae87071839
cchris37 <57228138+cchris37@users.noreply.github.com>
2020-01-20 20:52:45 +0100

    Templates
    * added css for the template
    * added about-page/base/home
    * modified views
    * added Blogconfig app to settings

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def name():
-	return "Christian's"
+#def name():
+#	return "Christian's"
 
-def putin_h1(string):
-	return "<h1>{}</h1>".format(string)
+#def putin_h1(string):
+#	return "<h1>{}</h1>".format(string)
 
-def home (request):
-	return HttpResponse (putin_h1("{} Blog Home".format(name())))
+#def home (request):
+#	return HttpResponse (putin_h1("{} Blog Home".format(name())))
 
-def about (recuest):
-	return HttpResponse (putin_h1("{} Blog About".format(name())))
+#def about (recuest):
+#	return HttpResponse (putin_h1("{} Blog About".format(name())))
 
+posts =[
+
+{
+'author': 'Christian',
+'title':  "Christian's Blog Post 1",
+'content': 'First post content',
+'date_posted': 'Dezember 2019'
+},
+{
+'author': 'Christian 2',
+'title':  "Christian's Blog Post 2",
+'content': 'Second post content',
+'date_posted': 'Januar 2020',
+}
+]
+
+
+def home(request):
+	context = {
+		'posts': posts
+	}
+	return render (request, 'blog/home.html', context)
+
+def about (request):
+	return render (request, 'blog/about.html', {'title': 'About'})
 
 # Create your views here.
